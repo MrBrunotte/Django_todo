2e640e27178aba0b8eef642255d831e2b9d80cb2
mrbrunotte <mrbrunotte@gmail.com>
2020-02-22 17:17:02 +0100

    Add allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5grjqnm7575f8kc-g@)_3g+c5m_tla0tnjcttznnbunrmkjsh#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-django-todo-app.herokuapp.com']
 
 
 # Application definition
